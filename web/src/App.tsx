439fc74ae6c08c4b055147febd7ea3f432e37d03
DiegoAmaxmandro <102663353+DiegoAmaxmandro@users.noreply.github.com>
2024-02-21 19:59:29 +0000

    Adding pulic, src and assets folder on web

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,12 @@
-function Button() {
-  return <button>Enviar</button>;
-}
+import "./assets/styles/main.css";
+//import logoImg from "./src/assets/game-4.png";
 
 function App() {
-  return <Button />;
+  return (
+    <div className="max-w-[1344px] mx-auto flex-col items-center my-20 ">
+      <img src="" alt="" />
+    </div>
+  );
 }
 
 export default App;
